e897dd8bc8562109500e36e015d512ba3f966f3b
Rıdvan Dikmen <ridvandikmen1997@gmail.com>
2023-10-11 14:53:11 +0300

    Initial Commit

---
+++ MP_DocumentCreate/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MP_DocumentCreate
+//
+//  Created by Rıdvan Dikmen on 11.10.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ MP_DocumentCreateUITests/MP_DocumentCreateUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  MP_DocumentCreateUITestsLaunchTests.swift
+//  MP_DocumentCreateUITests
+//
+//  Created by Rıdvan Dikmen on 11.10.2023.
+//
+
+import XCTest
+
+final class MP_DocumentCreateUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
